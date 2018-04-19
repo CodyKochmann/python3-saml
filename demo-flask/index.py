3a5566ea804313a229829ad28eeb5d1ece1496ac
Cody Kochmann <kochmanncody@gmail.com>
2018-04-19 17:43:31 -0400

    Update index.py
    did a little bit of cleanup... still needs more

--- demo-flask/index.py
+++ demo-flask/index.py
@@ -1,13 +1,11 @@
-import os
 
-from flask import (Flask, request, render_template, redirect, session,
-                   make_response)
-
-from urlparse import urlparse
-
+from flask import (Flask, request, render_template, redirect, session, make_response)
 from onelogin.saml2.auth import OneLogin_Saml2_Auth
 from onelogin.saml2.utils import OneLogin_Saml2_Utils
+from urlparse import urlparse
+import os
 
+''' example of a python saml flask server '''
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'onelogindemopytoolkit'
@@ -15,29 +13,24 @@ app.config['SAML_PATH'] = os.path.join(os.path.dirname(os.path.dirname(__file__)
 
 
 def init_saml_auth(req):
-    auth = OneLogin_Saml2_Auth(req, custom_base_path=app.config['SAML_PATH'])
-    return auth
-
+    return OneLogin_Saml2_Auth(req, custom_base_path=app.config['SAML_PATH'])
 
 def prepare_flask_request(request):
     # If server is behind proxys or balancers use the HTTP_X_FORWARDED fields
-    url_data = urlparse(request.url)
     return {
         'https': 'on' if request.scheme == 'https' else 'off',
         'http_host': request.host,
-        'server_port': url_data.port,
+        'server_port': urlparse(request.url).port,
         'script_name': request.path,
         'get_data': request.args.copy(),
         'post_data': request.form.copy()
     }
-
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
     req = prepare_flask_request(request)
     auth = init_saml_auth(req)
     errors = []
-    not_auth_warn = False
     success_slo = False
     attributes = False
     paint_logout = False
@@ -45,22 +38,22 @@ def index():
     if 'sso' in request.args:
         return redirect(auth.login())
     elif 'sso2' in request.args:
-        return_to = '%sattrs/' % request.host_url
-        return redirect(auth.login(return_to))
+        return redirect(
+            auth.login(
+                '{}attrs/'.format(request.host_url)
+            )
+        )
     elif 'slo' in request.args:
-        name_id = None
-        session_index = None
-        if 'samlNameId' in session:
-            name_id = session['samlNameId']
-        if 'samlSessionIndex' in session:
-            session_index = session['samlSessionIndex']
+        return redirect(
+            auth.logout(
+                name_id=session.get('samlNameId', None),
+                session_index=session.get('samlSessionIndex', None)
+            )
+        )
 
-        return redirect(auth.logout(name_id=name_id, session_index=session_index))
     elif 'acs' in request.args:
         auth.process_response()
-        errors = auth.get_errors()
-        not_auth_warn = not auth.is_authenticated()
-        if len(errors) == 0:
+        if len(auth.get_errors()) == 0:
             session['samlUserdata'] = auth.get_attributes()
             session['samlNameId'] = auth.get_nameid()
             session['samlSessionIndex'] = auth.get_session_index()
@@ -68,24 +61,22 @@ def index():
             if 'RelayState' in request.form and self_url != request.form['RelayState']:
                 return redirect(auth.redirect_to(request.form['RelayState']))
     elif 'sls' in request.args:
-        dscb = lambda: session.clear()
-        url = auth.process_slo(delete_session_cb=dscb)
-        errors = auth.get_errors()
-        if len(errors) == 0:
-            if url is not None:
-                return redirect(url)
-            else:
+        url = auth.process_slo(delete_session_cb=session.clear)
+        if len(auth.get_errors()) == 0:
+            if url is None:
                 success_slo = True
+            else:
+                return redirect(url)
 
     if 'samlUserdata' in session:
         paint_logout = True
         if len(session['samlUserdata']) > 0:
-            attributes = session['samlUserdata'].items()
+            attributes = session['samlUserdata'].items() # otherwise this is False?
 
     return render_template(
         'index.html',
         errors=errors,
-        not_auth_warn=not_auth_warn,
+        not_auth_warn=(not auth.is_authenticated()),
         success_slo=success_slo,
         attributes=attributes,
         paint_logout=paint_logout
@@ -94,32 +85,28 @@ def index():
 
 @app.route('/attrs/')
 def attrs():
-    paint_logout = False
-    attributes = False
-
-    if 'samlUserdata' in session:
-        paint_logout = True
-        if len(session['samlUserdata']) > 0:
-            attributes = session['samlUserdata'].items()
-
-    return render_template('attrs.html', paint_logout=paint_logout,
-                           attributes=attributes)
+    return render_template(
+        'attrs.html',
+        paint_logout='samlUserdata' in session,
+        attributes=session['samlUserdata'].items() if 'samlUserdata' in session and len(session['samlUserdata']) > 0 else False
+    )
 
 
 @app.route('/metadata/')
 def metadata():
-    req = prepare_flask_request(request)
-    auth = init_saml_auth(req)
-    settings = auth.get_settings()
+    settings = init_saml_auth(
+        prepare_flask_request(request)
+    ).get_settings()
+
     metadata = settings.get_sp_metadata()
     errors = settings.validate_metadata(metadata)
 
-    if len(errors) == 0:
+    if len(errors):
+        return make_response(errors.join(', '), 500)
+    else:
         resp = make_response(metadata, 200)
         resp.headers['Content-Type'] = 'text/xml'
-    else:
-        resp = make_response(errors.join(', '), 500)
-    return resp
+        return resp
 
 
 if __name__ == "__main__":
